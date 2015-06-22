cdf8db251edb112e9d4f10dd434355106749e2f2
Felix Müller <mueller.fe@gmail.com>
2015-06-22 23:55:56 +0200

    40

--- ex8/ex40.pl
+++ ex8/ex40.pl
@@ -13,3 +13,13 @@ line(b, d). line(d, c). line(d, e).
  * Beachten Sie ausserdem, dass die obige "line"-Relation nicht symmetrisch ist,
  * Verbindungen zwischen Punkten aber natuerlich konzeptionell doch.
  */
+
+line1(A,B) :- line(A,B); line(B, A).
+triangle1(A,B,C) :- line1(A,B), line1(A,C), line1(B,C).
+
+triangle(A, B, C) :- triangle1(A,B,C), A\=B, B\=C, A\=C.
+
+tetragon1(A,B,C, D) :- line1(A,B), line1(B,C), line1(C,D), line1(D,A).
+tetragon(A, B, C, D) :- tetragon1(A,B,C,D), A\=B, B\=C, A\=C, A\=D, B\=D, C\=D.
+
+
